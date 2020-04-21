466faa0e927573bb3ef7a27e01892f24ac6fb5ba
Guillaume Bertello <guillaume.bertello@gmail.com>
2020-04-22 01:16:21 +0200

    Refactor test and app

--- python-hello-world/test.sh
+++ python-hello-world/test.sh
@@ -1,3 +1,3 @@
-docker build -t ${PWD##*/}-test -f test/Dockerfile . > /dev/null
+docker build -t ${PWD##*/}-test -f Dockerfile-test . > /dev/null
 docker run -t -e "TERM=xterm-256color" ${PWD##*/}-test
 
--- python-hello-world/test/test.py
+++ python-hello-world/test/test.py
@@ -5,5 +5,5 @@ def run_file(filename):
     return subprocess.check_output(["python", filename]).decode("utf-8").strip()
 
 def test_main():
-    assert run_file("main.py") == "Hello, World!"
+    assert run_file("/app/main.py") == "Hello, World!"
 
