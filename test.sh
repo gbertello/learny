1530e243c4c33032c08cc502e43f6dd71d8f0de6
Guillaume Bertello <guillaume.bertello@gmail.com>
2020-04-24 10:58:25 +0200

    Delete test for old platforms

--- test.sh
+++ test.sh
@@ -1,29 +1,9 @@
-echo "Testing python-hello-world"
-python-hello-world/test.sh
-echo ""
-
-echo "Testing python-parameter"
-python-parameter/test.sh
-echo ""
-
-echo "Testing python-parameters"
-python-parameters/test.sh
-echo ""
-
-echo "Testing python-getopt"
-python-getopt/test.sh
-echo ""
-
-echo "Testing python-disk"
-python-disk/test.sh
+echo "Testing flask"
+flask/test.sh
 echo ""
 
 echo "Testing mongo"
 mongo/test.sh
-echo ""
-
-echo "Testing flask"
-flask/test.sh
 echo ""
 
 echo "Testing node"
