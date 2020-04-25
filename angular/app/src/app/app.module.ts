ebf3a60b90907b5b5abf2660bb30d48a1ccb10fa
Guillaume Bertello <guillaume.bertello@gmail.com>
2020-04-25 03:46:30 +0200

    Simplify angular template

--- angular/app/src/app/app.module.ts
+++ angular/app/src/app/app.module.ts
@@ -1,24 +1,14 @@
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { StandingsComponent } from './standings/standings.component';
-import { SummaryComponent } from './summary/summary.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    StandingsComponent,
-    SummaryComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular/test/test.py
+++ angular/test/test.py
@@ -4,4 +4,3 @@ import requests
 def test_main():
   r = requests.get("http://learny_angular_test:80/").text.strip()
   assert r.startswith("<!doctype html>")
-  
