451f0edb32f1f3e28016bf779b67c75f0e47e31e
Guillaume Bertello <guillaume.bertello@gmail.com>
2021-04-28 22:24:16 +0200

    Refactor flask

---
+++ flask/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import os
+import requests
+
+def test_main():
+  r = requests.get("http://localhost:3000/").text.strip()
+  assert r == "Hello World!"
+
+if __name__ == "__main__":
+  import subprocess
+  subprocess.run(["pytest", __file__])
--- flask/test/test.py
+++
@@ -1,7 +0,0 @@
-import os
-import requests
-
-
-def test_main():
-  r = requests.get("http://%s:80/" % (os.getenv("IMAGE").replace("pytest", "test"))).text.strip()
-  assert r == "Hello World!"
