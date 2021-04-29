c9da7bcb5e9e3c62af97dde1367e5e51ca6b10e3
Guillaume Bertello <guillaume.bertello@gmail.com>
2021-04-29 21:31:14 +0200

    Add docker ps in stop

--- mongo/stop.py
+++ mongo/stop.py
@@ -6,3 +6,5 @@ import subprocess as sp
 
 sp.run(["docker", "stop", IMAGE], stdout=open(os.devnull, 'w'), stderr=open(os.devnull, 'w'))
 sp.run(["docker", "rm", IMAGE], stdout=open(os.devnull, 'w'), stderr=open(os.devnull, 'w'))
+
+sp.run(["docker", "ps"])
