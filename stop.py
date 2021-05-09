3b8471a73a85fa6c8ac8ec016f0245ef50b30de8
Guillaume Bertello <guillaume.bertello@gmail.com>
2021-05-09 18:22:55 +0200

    Propagate arguments for app deployment

--- stop.py
+++ stop.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import os
+import sys
 import subprocess as sp
 
 CWD = os.path.dirname(os.path.abspath(__file__))
 for dirname in os.listdir(CWD):
   script_name = os.path.join(os.path.join(CWD, dirname), "stop.py")
   if os.path.exists(script_name):
-    sp.run([script_name])
+    sp.run([script_name] + sys.argv[1:])
