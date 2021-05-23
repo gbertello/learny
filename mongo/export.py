61e2d274f08303ce5d00eb10a30ad71b97796bc4
Guillaume Bertello <guillaume.bertello@gmail.com>
2021-05-23 22:30:16 +0200

    Fix mongo import and export

--- mongo/export.py
+++ mongo/export.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
+import argparse
 import os
 import subprocess as sp
-from config import CWD, IMAGE
+
+CWD = os.path.dirname(os.path.abspath(__file__))
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--system", default="dev", help="Specify system")
+args = parser.parse_args()
+SYSTEM = args.system
+
+IMAGE = os.path.basename(os.path.dirname(CWD)) + "_" + os.path.basename(CWD) + "_" + SYSTEM
 
 db = "test_db"
-collection = "collection"
-filename = os.path.join(CWD, "data", f"{collection}_export")
-target_filename = f"/tmp/{collection}_export"
+collections = ["collection"]
 
+for collection in collections:
+  filename = os.path.join(CWD, "data", f"{collection}_export")
+  target_filename = f"/tmp/{collection}_export"
 
-sp.run(["docker", "exec", IMAGE, "mongoexport", "--db", db, "--collection", collection, f"--out={target_filename}"])
-sp.run(["docker", "cp", f"{IMAGE}:{target_filename}", filename])
-sp.run(["docker", "exec", IMAGE, "rm", target_filename])
+  sp.run(["docker", "exec", IMAGE, "mongoexport", "--db", db, "--collection", collection, f"--out={target_filename}"])
+  sp.run(["docker", "cp", f"{IMAGE}:{target_filename}", filename])
+  sp.run(["docker", "exec", IMAGE, "rm", target_filename])
--- mongo/import.py
+++ mongo/import.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python3
+import argparse
 import os
 import subprocess as sp
-from config import CWD, IMAGE
+
+
+CWD = os.path.dirname(os.path.abspath(__file__))
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--system", default="dev", help="Specify system")
+args = parser.parse_args()
+SYSTEM = args.system
+
+IMAGE = os.path.basename(os.path.dirname(CWD)) + "_" + os.path.basename(CWD) + "_" + SYSTEM
 
 db = "test_db"
-collection = "collection"
-filename = os.path.join(CWD, "data", f"{collection}_import")
-target_filename = f"/tmp/{collection}_import"
+collections = ["collection"]
 
-sp.run(["docker", "exec", IMAGE, "mongo", db, "--eval", "db.dropDatabase()"], stdout=open(os.devnull, 'w'))
+sp.run(["docker", "exec", f"learny_mongo_{SYSTEM}", "mongo", db, "--eval", "db.dropDatabase()"], stdout=open(os.devnull, 'w'))
 
-sp.run(["docker", "cp", filename, f"{IMAGE}:{target_filename}"])
-sp.run(["docker", "exec", IMAGE, "mongoimport", "--db", db, "--collection", collection, "--file", target_filename])
-sp.run(["docker", "exec", IMAGE, "rm", target_filename])
+for collection in collections:
+  filename = os.path.join(CWD, "data", SYSTEM, f"{collection}")
+  target_filename = f"/tmp/{collection}_import"
+
+  sp.run(["docker", "cp", filename, f"{IMAGE}:{target_filename}"])
+  sp.run(["docker", "exec", f"{IMAGE}", "mongoimport", "--db", db, "--collection", collection, "--file", target_filename])
+  sp.run(["docker", "exec", f"{IMAGE}", "rm", target_filename])
